fc3ce3c32a0b8e112bffeeaab9ea189da2941490
Sebm71 <77967189+Sebm71@users.noreply.github.com>
2021-04-14 22:06:29 +0200

    Add files via upload

---
+++ bootspringdemo/src/main/java/com/bankapi/demo/DemobankapiApplication.java
@@ -0,0 +1,13 @@
+package com.bankapi.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemobankapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemobankapiApplication.class, args);
+	}
+
+}
---
+++ bootspringdemo/src/test/java/com/bankapi/demo/DemobankapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bankapi.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemobankapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
