8e662bbc5545c90fc52a64ebc15697205daf2937
Sebm71 <77967189+Sebm71@users.noreply.github.com>
2021-05-03 22:14:59 +0200

    Add files via upload

---
+++ accountservice.java
@@ -0,0 +1,34 @@
+package com.example;
+
+public class accountservice
+import java.util.ArrayList;
+        import java.util.Collection;
+        import java.util.Date;
+        import java.util.UUID;
+
+        import org.springframework.beans.factory.annotation.Autowired;
+        import org.springframework.stereotype.Service;{
+        import com.example.entities.Address;
+        import com.example.entities.Account
+        import com.example.repositories.AccountInMemoryRepository;
+        import com.example.requests.AddAccountRequest;
+        import com.example.requests.AddAccountRequest;
+        import com.example.requests.UpdateAccountRequest;
+
+@Service
+public class Accountservice {
+    public <Accountservice> getAll()
+}return Account.getAll();
+
+public Account getById(UUID id) {
+        return Account.getById(id);
+
+public void deleteAccount(UUID id) {
+        AccounttRepo.deleteAccount(id);
+        }
+
+
+
+
+
+        
