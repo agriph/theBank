5ce121729734f4edbc3d863301d4ec14cc8d67fc
Agnes Griph <agngriph@gmail.com>
2021-05-16 22:28:53 +0200

    AccountController
    Updated with descriptions using @ApiOperation (value, notes and response) to clarify the purpose of each method.

--- AccountController.java
+++ AccountController.java
@@ -40,28 +40,48 @@ public class AccountController {
 	}
 	
 	@GetMapping("/{id}")
+	@ApiOperation(
+			value = "Get account by Id",
+			notes = "View account info by using account Id",
+			response = Account.class)
 	public Account getById(@ApiParam (value = "ID of accounts", required = true) @PathVariable UUID id) {
 		return accountService.getById(id);
 	}
 	
 	@GetMapping("/{name}")
+	@ApiOperation(
+			value = "Get account by first- or lastname",
+			notes = "View account info using either first- or lastname of the accountholder",
+			response = Account.class)
 	public Account getByName(@ApiParam(value = "Name of Accountholder", required = true) @PathVariable String firstName, String lastName) {
 	return accountService.getByName(firstName, lastName);
 	}
 	
 	@PostMapping("/")
+	@ApiOperation(
+			value = "Add new account",
+			notes = "Add a new account to the bank API",
+			response = Account.class)
 	public Account addNewAccount(@RequestBody AddAccountRequest request) {
 		return accountService.addAccount(request);
 	}
 		
 	
 	@PutMapping("/{id}")
+	@ApiOperation(
+			value = "Update account",
+			notes = "Update account with new information",
+			response = Account.class)
 	public Account updateAccount(@PathVariable UUID id, @RequestBody UpdateAccountRequest request) {
 		return accountService.updateAccount(request, id);
 	}
 
 	
 	@DeleteMapping("/{id}")
+	@ApiOperation(
+			value = "Delete account",
+			notes = "This removes the account from the bank API",
+			response = Account.class)
 	public void deleteAccount(@PathVariable UUID id) {
 		accountService.deleteAccount(id);
 	}
