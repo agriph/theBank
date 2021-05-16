1f1eb9a94b4d69b6d73515b4d08d3ac63b5490c4
Agnes Griph <agngriph@gmail.com>
2021-05-16 22:38:04 +0200

    AccountService
    Changed a few things regarding placement of the code.

--- AccountService.java
+++ AccountService.java
@@ -1,62 +1,62 @@
-		package com.bank.services;
+package com.bank.services;
 
-		import java.util.Collection;
-        import java.util.Date;
-        import java.util.UUID;
+import java.util.Collection;
+import java.util.Date;
+import java.util.UUID;
 
-        import org.springframework.beans.factory.annotation.Autowired;
-        import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
-        import com.bank.entities.Account;
-        import com.bank.repositories.AccountRepository;
-        import com.bank.requests.AddAccountRequest;
-        import com.bank.requests.UpdateAccountRequest;    
+import com.bank.entities.Account;
+import com.bank.repositories.AccountRepository;
+import com.bank.requests.AddAccountRequest;
+import com.bank.requests.UpdateAccountRequest;    
 
-        @Service
-        public class AccountService {
+@Service
+public class AccountService {
         	
-        @Autowired
-        AccountRepository accountRepo;
+@Autowired
+AccountRepository accountRepo;
         
-        UUID id = UUID.randomUUID();
+UUID id = UUID.randomUUID();
             
-        public Collection<Account> getAll() {
-        return accountRepo.getAll();
-        }
+	public Collection<Account> getAll() {
+	return accountRepo.getAll();
+	}
 
-        public Account getById(UUID id) {
-        return accountRepo.getById(id);
-        }
+	public Account getById(UUID id) {
+	return accountRepo.getById(id);
+	}
         
-		public Account getByName(String firstName, String lastName) {
-		return accountRepo.getByName(firstName, lastName);
-		}
+	public Account getByName(String firstName, String lastName) {
+	return accountRepo.getByName(firstName, lastName);
+	}
 
-        public void deleteAccount(UUID id) {
-        accountRepo.deleteAccount(id);
-        }
+	public void deleteAccount(UUID id) {
+	accountRepo.deleteAccount(id);
+	}
 
-		public Account addAccount(AddAccountRequest request) {
-		 Account account = new Account();
-		 account.setId(UUID.randomUUID());
-		 account.setFirstName(request.getFirstName());
-		 account.setLastName(request.getLastName());
-		 account.setAge(request.getAge());
-		 account.setCity(request.getCity());
-		 account.setDate(request.getDate());
+	public Account addAccount(AddAccountRequest request) {
+	Account account = new Account();
+	account.setId(UUID.randomUUID());
+	account.setFirstName(request.getFirstName());
+	account.setLastName(request.getLastName());
+	account.setAge(request.getAge());
+	account.setCity(request.getCity());
+	account.setDate(request.getDate());
 		 
-		 return accountRepo.addAccount(account);
-		}
+	return accountRepo.addAccount(account);
+	}
 
-		public Account updateAccount(UpdateAccountRequest request, UUID id) {
-			Account account = accountRepo.getById(id);
-			account.setFirstName(request.getFirstName());
-			account.setLastName(request.getLastName());
-			account.setAge(request.getAge());
-			account.setCity(request.getCity());
-			account.setUpdate(new Date());
+	public Account updateAccount(UpdateAccountRequest request, UUID id) {
+	Account account = accountRepo.getById(id);
+	account.setFirstName(request.getFirstName());
+	account.setLastName(request.getLastName());
+	account.setAge(request.getAge());
+	account.setCity(request.getCity());
+	account.setUpdate(new Date());
 			
-			return accountRepo.updateAccount(id, account);
-		}
+	return accountRepo.updateAccount(id, account);
+	}
 		
-   }
+}
